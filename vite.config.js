baa516c7bccbd27526faefeec64c7b2992089582
Prathibha <121505005+prathibha-bollu@users.noreply.github.com>
2025-09-04 14:39:52 +0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: './Speech_to_text', // important if you want to deploy on GitHub Pages or a subfolder
-});
